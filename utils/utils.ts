3fc39d54af6ddc625fcd8e3577a7929229e236eb
eren <erenkuek1106@gmail.com>
2024-03-12 20:36:17 +0800

    use moment package to display time ago

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx"
+import moment from "moment";
 import { twMerge } from "tailwind-merge"
 
 // for tailwind
@@ -9,31 +10,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const convertDateToTimeAgoStr = (date: Date | string): string => {
   const receivedDate = typeof date === 'string' ? new Date(date) : date;
-  const now = new Date();
-  const seconds = Math.floor((now.getTime() - receivedDate.getTime()) / 1000);
-
-  let interval = Math.floor(seconds / 31536000);
-
-  if (interval > 1) {
-    return `${interval} years ago`;
-  }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return `${interval} months ago`;
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return `${interval} days ago`;
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return `${interval} hours ago`;
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return `${interval} minutes ago`;
-  }
-  return "just now";
+  return moment(receivedDate).fromNow();
 }
 
 export const convertIdTitleToSlug = (id: number, title: string) => {
