d0f256a33b654c7122054dd3980560501db3832c
eren <erenkuek1106@gmail.com>
2024-03-13 00:02:10 +0800

    Show progress bar at top while switching page

--- components/client-provider.tsx
+++ components/client-provider.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import { SessionProvider } from "next-auth/react"
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
 
-export default function Provider ({
+export default function Provider({
   children,
   session
 }: {
@@ -10,6 +11,7 @@ export default function Provider ({
   session: any
 }): React.ReactNode {
   return <SessionProvider session={session}>
+    <ProgressBar />
     {children}
   </SessionProvider>
 }
